b4b3039e32fbf88c585b80266a3f58b670ac16cb
Shane <sb_94@yahoo.com>
2023-08-15 17:08:52 -0700

    added logic to putDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,17 +13,34 @@ const initdb = async () =>
   });
 
   
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.log('PUT from the database');
 
-// TODO: Add logic for a method that gets all the content from the database
+// Create connection to database 'jate' and version '1'
+const jateDb = await openDB('jate', 1);
+
+// Create a new transaction on 'jate' object store with data privileges
+const tx = jateDb.transaction('jate', 'readwrite');
+
+// Open up the desired object store
+const store = tx.objectStore('jate');
+
+// Use put method to update data in the object store
+await store.put({ id: 1, content });
+
+// Complete the transaction
+await tx.complete;
+};
+
+
 export const getDb = async () => {
   console.log('GET from the database');
 
 // Create connection to database 'jate' and version '1'
-  const jateDB = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify db name and data privileges
-  const tx = jateDB.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
 // Open up the desired object store.
   const store = tx.objectStore('jate');
